b768f3e9f4646502e7989f48e687e107d947045b
Joshua Briggs <joshuabriggs0001@gmail.com>
2025-01-24 14:48:37 +0000

    todo-project init

---
+++ api/TodoApp/models.py
@@ -0,0 +1,13 @@
+from database import base
+from sqlalchemy import Boolean, Column, Integer, String
+
+
+class Todos(base):
+    __tablename__ = 'todos'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    priority = Column(Integer)
+    complete = Column(Boolean, default=False)
+    
--- api/api/database.py
+++
@@ -1,3 +0,0 @@
-from sqlalchemy import create_engine
-
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
